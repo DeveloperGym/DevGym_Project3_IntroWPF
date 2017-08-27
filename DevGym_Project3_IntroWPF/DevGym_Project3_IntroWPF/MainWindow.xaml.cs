2145f24bf78ad1c230a6174f4e254108c96b7d18
kennebel <jac@kennebel.com>
2017-08-26 20:42:43 -0600

    Working eye animation

---
+++ DevGym_Project3_IntroWPF/DevGym_Project3_IntroWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DevGym_Project3_IntroWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Properties
+        public DispatcherTimer EyeUpdater { get; set; }
+
+        public List<EyeMotion> Motions { get; set; }
+        #endregion
+
+        #region Construct / Destruct
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Simple Eye Animation
+            Motions = new List<EyeMotion>();
+            Motions.Add(new EyeMotion(Eye0, TheEye));
+            Motions.Add(new EyeMotion(Eye1, TheEye));
+
+            EyeUpdater = new DispatcherTimer();
+            EyeUpdater.Tick += EyeUpdater_Tick;
+            EyeUpdater.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            EyeUpdater.Start();
+        }
+        #endregion
+
+        #region Events
+        private void EyeUpdater_Tick(object sender, EventArgs e)
+        {
+            Motions.ForEach(eye => eye.Update());
+        }
+        #endregion
+    }
+}
---
+++ DevGym_Project3_IntroWPF/DevGym_Project3_IntroWPF/Support/EyeMotion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace DevGym_Project3_IntroWPF
+{
+    public class EyeMotion
+    {
+        #region Properties
+        public Shape ObjectToMove { get; set; }
+        public Canvas Parent { get; set; }
+
+        public double MoveSpeed { get; set; }
+
+        public bool MovingRight { get; set; }
+        #endregion
+
+        #region Construct / Destruct
+        public EyeMotion(Shape objectToMove, Canvas parent, double moveSpeed = 1d, bool movingRight = true)
+        {
+            // Required Parameters
+            ObjectToMove = objectToMove;
+            Parent = parent;
+
+            // Optional Parameters
+            MoveSpeed = moveSpeed;
+            MovingRight = movingRight;
+        }
+        #endregion
+
+        #region Methods
+        public void Update()
+        {
+            var Left = Canvas.GetLeft(ObjectToMove);
+            if (MovingRight)
+            {
+                Left += MoveSpeed;
+                var MaxLeft = (Parent.ActualWidth - ObjectToMove.ActualWidth);
+                if (Left > MaxLeft)
+                {
+                    Left = MaxLeft;
+                    MovingRight = false;
+                }
+            }
+            else
+            {
+                Left -= MoveSpeed;
+                if (Left <= 0)
+                {
+                    Left = 0;
+                    MovingRight = true;
+                }
+            }
+            Canvas.SetLeft(ObjectToMove, Left);
+        }
+        #endregion
+    }
+}
